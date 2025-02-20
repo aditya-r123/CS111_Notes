b8ede7fa60a2e8b3822b0e2ec67c860ed120ab68
Aditya Rao <54918123+aditya-r123@users.noreply.github.com>
2025-02-19 23:56:43 -0800

    Update tower.pl

--- tower.pl
+++ tower.pl
@@ -1,14 +1,16 @@
 % 1. ntower
+%N, a nonnegative integer specifying the size of the square grid.
+%T, a list of N lists, each representing a row of the square grid. Each row is represented by a list of N distinct integers from 1 through N. The corresponding columns also contain all the integers from 1 through N.
+%C, a structure with function symbol counts and arity 4. Its arguments are all lists of N integers, and represent the tower counts for the top, bottom, left, and right edges, respectively.
 
 ntower(N, T, C) :-
-    check_rowlength(T, N),
-    check_columnlength(T, N),
-    check_domain(T, N),
-    maplist(fd_all_different, T),
-    transpose(T, TT),
-    maplist(fd_all_different, TT),
-    C = counts(Top, Bot, Left, Right),
-    check_edge(TT, Top),
-    check_edge_reverse(TT, Bot),
-    check_edge(T, Left),
-    check_edge_reverse(T, Right).
+    row_len(T, N),
+    col_len(T, N),
+
+row_len(T, N) :- 
+    length(T, N).
+    
+col_len([], _).
+col_len([H | T], N) :-
+    length(H, N),
+    col_len(T, N).

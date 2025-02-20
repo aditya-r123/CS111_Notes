e9cba6792912aa7417adebd6f1edb189716eb186
Aditya Rao <54918123+aditya-r123@users.noreply.github.com>
2025-02-19 23:46:35 -0800

    Update tower.pl

--- tower.pl
+++ tower.pl
@@ -1,5 +1,14 @@
 % 1. tower
+
 tower(N, T, C) :-
     check_rowlen(T, N),
     check_collen(T, N),
     check_domain(T, N),
+    maplist(fd_all_different, T),
+    transpose(T, TT),
+    maplist(fd_all_different, TT),
+    C = counts(Top, Bot, Left, Right),
+    check_edge(TT, Top),
+    check_edge_rev(TT, Bot),
+    check_edge(T, Left),
+    check_edge_rev(T, Right).
